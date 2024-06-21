0f443297777309de68ef88d62512e553adcfd407
Hugo Luis Ruscitti <hugo.ruscitti@mercadolibre.com>
2024-06-21 15:02:42 -0300

    Mejorando estilo del editor.

--- js/canvas.js
+++ js/canvas.js
@@ -1,13 +1,16 @@
 import Scene from "./scene.js";
 
 class Canvas extends HTMLElement {
+
   connectedCallback() {
+    this.innerHTML = "<div id='canvas'></div>";
     const config = {
       type: Phaser.AUTO,
       width: 128,
       height: 128,
       backgroundColor: '#777777',
       pixelArt: true,
+      parent: 'canvas',
       scene: Scene
     };
 
