9cea12d8a1e249d57fb38709b023e08d24d4ea09
Jose Cabrera <murdoc.jose.6@gmail.com>
2024-06-21 12:57:04 -0300

    feat: added drawLine method to change color line

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function run() {
   filbert.defaultOptions.runtimeParamName = "py";
 
   const opciones = {
-    locations: false, 
+    locations: false,
     ranges: false,
   }
 
@@ -85,8 +85,8 @@ function run() {
 
   filbert.pythonRuntime.functions.print = print;
 
-  const dibujar = canvas.dibujar.bind(canvas);
   const clear = canvas.clear.bind(canvas);
+  const drawLine = canvas.drawLine.bind(canvas);
 
   let eval_string = "(function(py){" + js + "})(filbert.pythonRuntime);"
   eval(eval_string);
--- js/scene.js
+++ js/scene.js
@@ -1,20 +1,31 @@
 // VER: https://newdocs.phaser.io/docs/3.55.1/Phaser.GameObjects.Graphics
 
-class Scene extends Phaser.Scene
-{
+class Scene extends Phaser.Scene {
+  constructor() {
+    super({ key: "Scene" });
+    this.bitColors = {
+      black: 0x000000,
+      blue: 0x0000ff,
+      green: 0x00ff00,
+      red: 0xff0000,
+      white: 0xffffff,
+    }
+  }
   preload() {
     this.graphics = this.add.graphics(200, 200);
   }
 
   create() {
     window.canvas = this;
-    window.dibujar = () => {this.dibujar();};
   }
 
-  dibujar(x, y, x2, y2) {
-    let graphics = this.graphics;
+  getColor(key, defaultValue = 0x000000) {
+    return this.bitColors[key] || defaultValue;
+  }
 
-    graphics.lineStyle(1, 0x00ff00);
+  drawLine(x, y, x2, y2, color) {
+    let graphics = this.graphics;
+    graphics.lineStyle(1, this.getColor(color));
     graphics.beginPath();
     graphics.moveTo(x, y);
     graphics.lineTo(x2, y2);
